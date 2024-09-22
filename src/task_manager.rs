feaf2e3db490e133a3d8c64aa41ec36f855a6a4c
R366y <nicomn@gmail.com>
2024-09-22 19:58:27 +0200

    added new methods to Priority, those are necessary for sort by and print priorities on the task list.

--- src/task_manager.rs
+++ src/task_manager.rs
@@ -1,30 +1,71 @@
+use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 use crate::task_db::DB;
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Priority {
-    High = 3,
-    Medium = 2,
-    Low = 1,
+    Low,
+    Medium,
+    High,
 }
 
-impl Display for Priority{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+impl Priority {
+
+    pub fn next(&self) -> Self {
         match self {
-            Priority::High => write!(f, "High"),
-            Priority::Medium => write!(f, "Medium"),
-            Priority::Low => write!(f, "Low"),
+            Priority::Low => Priority::Medium,
+            Priority::Medium => Priority::High,
+            Priority::High => Priority::Low,
+        }
+    }
+
+    pub fn from_u8(value: u8) -> Option<Priority> {
+        match value {
+            0 => Some(Priority::Low),
+            1 => Some(Priority::Medium),
+            2 => Some(Priority::High),
+            _ => None,
+        }
+    }
+
+    pub fn to_usize(&self) -> usize {
+        match self {
+            Priority::Low => 0,
+            Priority::Medium => 1,
+            Priority::High => 2,
         }
     }
 }
 
-impl Priority {
-    pub fn from_u8(value: u8) -> Option<Priority> {
-        match value {
-            3 => Some(Priority::High),
-            2 => Some(Priority::Medium),
-            1 => Some(Priority::Low),
-            _ => None,
+impl From<&str> for Priority {
+    fn from(value: &str) -> Self {
+        match value.to_uppercase().as_str() {
+            "L" => Priority::Low,
+            "M" => Priority::Medium,
+            "H" => Priority::High,
+            _ => unreachable!("Invalid priority",),
         }
+    }
+}
+
+impl Display for Priority {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Priority::Low => write!(f, "L"),
+            Priority::Medium => write!(f, "M"),
+            Priority::High => write!(f, "H"),
+        }
+    }
+}
+
+impl PartialOrd<Self> for Priority {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Priority {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.to_usize().cmp(&other.to_usize())
     }
 }
 
